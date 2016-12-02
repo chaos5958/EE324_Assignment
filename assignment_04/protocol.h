2d336e8330dcd708b90b0debf7a1c0d11245d0af
Hyun Ho Yeo <chaos5958@gmail.com>
2016-12-02 02:49:55 -0800

    project 4 start

---
+++ assignment_04/protocol.h
@@ -0,0 +1,34 @@
+#include <linux/limits.h>
+
+#define HELLO_FROM_CHD_TO_SUP 0x10
+#define HELLO_FROM_SUP_TO_CHD 0x11
+#define HELLO_FROM_SUP_TO_SUP 0x12
+#define FILEINFO_FROM_CHD_TO_SUP 0x20
+#define FILEINFO_OKAY_FROM_SUP_TO_CHD 0x21
+#define FILEINDO_FAIL_FROM_SUP_TO_CHD 0x22
+#define SEARCHQRY_FROM_CHD_TO_SUP 0x30
+#define SEARCHQRY_OKAY_FROM_SUP_TO_CHD 0x31
+#define SEARCHQRY_FAIL_FORM_SUP_TO_CHD 0x32
+#define FILEREQ_FROM_FROM_CHD_TO_CHD 0x40
+#define FILEREQ_OKAY_FROM_CHD_TO_CHD 0x41
+#define FILEREQ_FAIL_FROM_CHD_TO_CHD 0x42
+#define FILEINFOSHR_FROM_SUP_TO_SUP 0x50
+#define FILEINFOSHR_OKAY_FROM_SUP_TO_SUP 0x51
+#define FILEINFOSHR_FAIL_FROM_SUP_TO_SUP 0x52
+
+typedef struct _kaza_msg_t {
+    int total_len;
+    int id;
+    int msd_type;
+    int data;
+} kaza_msg_t;
+
+typedef struct _file_info_t {
+    char name[NAME_MAX];
+    size_t size;
+} file_info_t;
+
+typedef struct _loc_info_t {
+    uint32_t ip;
+    uint16_t port;
+} location_info_t;
